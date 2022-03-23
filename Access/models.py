bdda3ae68b7a6f65b8da318277edd765bf4fe4a3
Kipkorir2017 <Kipkorir2017>
2022-03-23 12:37:34 +0300

    add class models and register them in admin file

--- Access/models.py
+++ Access/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    name=models.CharField(max_length=200)
+    JobNo=models.IntegerField(max_length=10)
+    def __str__(self):
+        return self.name
+        
+
+class Department(models.Model):
+    name=models.CharField(max_length=200)
+    Flournum=models.AutoField(max_length=3)
+    def __str__(self):
+        return self.name
+
+
